66445fcf16cffa79306732c74674f6c789688e70
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-09-27 13:43:35 +0200

    result list component.

---
+++ src/components/ResultList.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+const StyledResultList = styled.ul`
+`
+
+export default function ResultList() {
+  return (
+    <StyledResultList role="list">
+    </StyledResultList>
+  )
+}
